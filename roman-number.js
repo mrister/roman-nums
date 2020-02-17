ed22a1fe37f3ef151b943e215689975207234a90
mrister <mihovil.rister@gmail.com>
2020-02-17 16:34:40 +0100

    Test setup for returned object shape and optional usage of 'new' and expanding the test file infrastructure.

--- roman-number.js
+++ roman-number.js
@@ -2,7 +2,7 @@
  *  A class/function to represent roman numbers and integers from roman numbers
  * @param {String |Number} val the value to be parsed and analysed, either an int or a string representing a roman numeral
  */
-function RomanNumber(val) {
+function RomanNumber (val) {
 
 }
 
--- test.js
+++ test.js
@@ -2,4 +2,32 @@ const { RomanNumber } = require('./roman-number')
 
 console.assert(typeof RomanNumber === 'function', 'RomanNumber is a function')
 
+// testing the shape of the returned object
+function validateShape (obj) {
+  if (typeof obj !== 'object') throw new Error('The returned value must be an object')
+  if (typeof obj.toInt !== 'function') throw new Error('The returned value must be an object that has toInto function')
+  if (typeof obj.toString !== 'function') throw new Error('The returned value must be an object that has toString function')
+  return true
+}
+
+function testShape () {
+  let roman = new RomanNumber('MC')
+  validateShape(roman)
+  roman = new RomanNumber(10)
+  validateShape(roman)
+
+  // validate without new
+  roman = RomanNumber('MCX')
+  validateShape(roman)
+  roman = RomanNumber(20)
+  validateShape(roman)
+  return true
+}
+
+function run () {
+  console.assert(testShape(), 'Invalid shape of the RomanNumbers returned object')
+}
+
+console.log('Tests Starting')
+run()
 console.log('Test done')
