c162691f559136f01139f6f3176e90dbe5e12e52
mrister <mihovil.rister@gmail.com>
2020-02-17 16:23:22 +0100

    Initial setup of lib main code and test infrastructure shell (file)

---
+++ roman-number.js
@@ -0,0 +1,11 @@
+/**
+ *  A class/function to represent roman numbers and integers from roman numbers
+ * @param {String |Number} val the value to be parsed and analysed, either an int or a string representing a roman numeral
+ */
+function RomanNumber(val) {
+
+}
+
+module.exports = {
+  RomanNumber
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+const { RomanNumber } = require('./roman-number')
+
+console.assert(typeof RomanNumber === 'function', 'RomanNumber is a function')
+
+console.log('Test done')
